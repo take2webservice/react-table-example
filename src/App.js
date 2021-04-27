1958aa9aaa680c89a00220f2307098767e80faba
BP 井関 健人 <taketo.iseki@staff.gu3.co.jp>
2021-04-27 12:13:25 +0900

    react-bootstrap-table-nextを使ってみる

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import BootstrapTable from 'react-bootstrap-table-next';
 import './App.css';
+
+const products = [
+  {
+    id: 1,
+    name: "食パン",
+    price: 500,
+  },
+  {
+    id: 2,
+    name: "お米",
+    price: 1000,
+  },
+  {
+    id: 3,
+    name: "パスタ",
+    price: 200,
+  },
+];
+const columns = [{
+  dataField: 'id',
+  text: '商品ID',
+  formatter: (cell, row) => (<div>{cell}</div>)
+}, {
+  dataField: 'name',
+  text: '商品名'
+}, {
+  dataField: 'price',
+  text: '価格'
+}];
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BootstrapTable keyField='id' data={products} columns={columns} />
     </div>
   );
 }
